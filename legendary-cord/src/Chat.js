f8484be7e58ec0416797edfcd638fa3900d78911
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-11-08 15:10:08 -0500

    Added Input Bar

--- legendary-cord/src/Chat.js
+++ legendary-cord/src/Chat.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import './Chat.css'
-import ChatHeader from './ChatHeader'
+import React from "react";
+import "./Chat.css";
+import ChatHeader from "./ChatHeader";
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import RedeemIcon from '@mui/icons-material/Redeem';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import FilterFramesIcon from '@mui/icons-material/FilterFrames';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 function Chat() {
   return (
     <div className="chat">
-        <ChatHeader />
+      <ChatHeader />
+
+      <div className="chat__messages"></div>
+
+
+      
+      <div className="chat__input">
+        <AddCircleRoundedIcon fontSize="large" />
+        <form>
+          <input placeholder={"Message #testchannel"} />
+          <button className="chat__inputButton" type="submit">Send Message</button>
+        </form>
+
+        <div className="chat__inputIcons">
+            <RedeemIcon fontSize="large"/>
+            <GifBoxIcon fontSize="large"/>
+            <FilterFramesIcon fontSize="large"/>
+            <EmojiEmotionsIcon fontSize="large"/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
